4d1687deb96efccd0ed1577435cb8a51989e0fb3
yuanqinglong <yuanql96@yeah.net>
2024-02-13 15:45:31 +0800

    fix(eslint): 调整prettier,eslint 配置冲突

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
-  api.cache(true)
+  api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['plugin:prettier/recommended', 'prettier'],
-  }
-}
+    presets: ["babel-preset-expo"],
+    plugins: ["plugin:prettier/recommended", "prettier"],
+  };
+};
